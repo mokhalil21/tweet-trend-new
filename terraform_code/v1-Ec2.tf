2572374d0567d2f7c0fbe57c7ae764fd3a430c93
mokhalil21 <mohamedcontact10@gmail.com>
2023-12-28 17:42:01 -0500

    add port 8080 to access jenkins

--- terraform_code/v1-Ec2.tf
+++ terraform_code/v1-Ec2.tf
@@ -34,6 +34,14 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "jenkins poer"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0

2bfc354225387d6981f2ae29a186f5ae933d8a6a
mokhalil21 <mohamedcontact10@gmail.com>
2024-01-04 13:07:39 -0500

    add terraform files and modules for eks

--- terraform_code/vpc/v1-Ec2.tf
+++ terraform_code/vpc/v1-Ec2.tf
@@ -117,3 +117,15 @@ subnet_id = aws_subnet.dpp-puplic-subnet-02.id
 route_table_id = aws_route_table.dpp-puplic-rt.id
   
 }
+
+  module "sgs" {
+    source = "../sg_eks"
+    vpc_id     =     aws_vpc.dpp-vpc.id
+ }
+
+  module "eks" {
+       source = "../eks"
+       vpc_id     =     aws_vpc.dpp-vpc.id
+       subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+       sg_ids = module.sgs.security_group_public
+ }
